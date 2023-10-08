eded0fdfe94dbc99d4ff47658bdf48fa617ed27b
saharrshirazii <137921636+saharrshirazii@users.noreply.github.com>
2023-10-08 15:49:47 +0200

    Create Composable.js

---
+++ React/It's Composable/Composable.js
@@ -0,0 +1,12 @@
+function MainContent() {
+    return (
+        <h1>I'm learning React!</h1>
+    )
+}
+
+ReactDOM.render(
+<div>
+    <MainContent />
+</div>,
+ document.getElementById("root")
+ )
