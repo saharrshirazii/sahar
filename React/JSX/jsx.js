a5c227fad3f0a1baff32d0606bb9f3ca720b8607
saharrshirazii <137921636+saharrshirazii@users.noreply.github.com>
2023-10-10 15:52:47 +0200

    Create jsx.js

---
+++ React/JSX/jsx.js
@@ -0,0 +1,17 @@
+const navbar = (
+    <nav>
+        <h1>My Website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>contact</li>
+
+        </ul>
+    </nav>
+)
+
+ReactDOM.render (navbar, document.getElementById("root"))
+
+//JSX converts HTML tags into react elements.
+//dar bala dar parantez az chand line HTML code estefade kardim ke haman JSX ast.
+
