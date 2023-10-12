80ff87c703c3cc5a549eaa7cfa177b0dc57d3457
saharrshirazii <137921636+saharrshirazii@users.noreply.github.com>
2023-10-12 14:25:44 +0200

    Create project-part1.js

---
+++ React/Project part1 - markup/project-part1.js
@@ -0,0 +1,20 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const page = (
+    <div>
+        <img src="./logo192.png" width="40px"/>
+        <h1>Fun Facts about React</h1>
+        <ul>
+            <li>was first released in 2013</li>
+            <li>was orginally created by Jordan Walke</li>
+            <li>has well over 100k stars on GitHub</li>
+            <li>is maintained by facebook</li>
+            <li>powers thousands of entereprise apps, including mobile apps</li>
+
+        </ul>
+    </div>
+)
+
+ReactDOM.render(page , document.getElementById("root"))
+
