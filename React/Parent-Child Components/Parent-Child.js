cdeee7ce0a8cd8a9d1aa7e7541264a441a044d1e
saharrshirazii <137921636+saharrshirazii@users.noreply.github.com>
2023-10-13 11:21:43 +0200

    Update Parent-Child.js

--- React/Parent-Child Components/Parent-Child.js
+++ React/Parent-Child Components/Parent-Child.js
@@ -8,9 +8,29 @@ function Header (){
                     <img src="./logo192.png" width="40px"/>
                 </nav>
         </header>
+    ) 
+}
 
+
+function MainContent(){
+    return(
+        <div>
+            <h1>Reason I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job s a developer if I know React</li>
+            </ol>
+        </div>
     )
-    
+}
+
+
+function Footer(){
+    return(
+        <footer>
+                @2023 Sahar developer. All rights reserved.
+         </footer>
+    )
 }
    
 
@@ -18,18 +38,12 @@ function TemporaryName(){
     return(
         <div>
             <Header/>
-            
-            <h1>Reason I'm excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job s a developer if I know React</li>
-            </ol>
-            <footer>
-                @2023 Sahar developer. All rights reserved.
-            </footer>    
-       </div>
+            <MainContent/>
+            <Footer/>    
+        </div>
     )
 }
+
 
 ReactDOM.render(<TemporaryName/> , document.getElementById("root"))
 
