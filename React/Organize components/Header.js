e6d3691aef0fea867697ce766bd71914ce8aeaa7
saharrshirazii <137921636+saharrshirazii@users.noreply.github.com>
2023-10-14 10:48:56 +0200

    Create Header.js

---
+++ React/Organize components/Header.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function Header (){
+    return(
+        <header>
+                <nav className ="nav">
+                    <img src="./logo192.png"/>
+                    <ul className ="nav-items">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+                </nav>
+               
+        </header>
+    ) 
+}
+
+export default Header
