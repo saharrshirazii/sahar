ae1d9b79488c2425ef7ecdbb2e916c87f616e3f6
saharrshirazii <137921636+saharrshirazii@users.noreply.github.com>
2023-10-14 10:47:55 +0200

    Create Footer.js

---
+++ React/Organize components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Footer(){
+    return(
+        <footer className="footer">
+                <div>@2023 Sahar developer. All rights reserved.</div>
+        </footer>
+    )
+}
+
+export default Footer
+   
