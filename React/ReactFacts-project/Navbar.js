01c90b2357e2f79692823cefcbb7aee25a3e541b
saharrshirazii <137921636+saharrshirazii@users.noreply.github.com>
2023-10-15 10:03:43 +0200

    Create Navbar.js

---
+++ React/ReactFacts-project/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './style.css';
+
+function Navbar (){
+return(
+    <nav className="nav"> 
+        <img src="logo192.png"/>
+        <h3>ReactFacts</h3>
+        <h4>React Course - Project 1</h4>
+    </nav>
+
+)
+}
+
+export default Navbar
