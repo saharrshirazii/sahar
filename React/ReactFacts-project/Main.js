c2922ed2c26d1a5d5b6302182fe624257fac8996
saharrshirazii <137921636+saharrshirazii@users.noreply.github.com>
2023-10-15 10:02:18 +0200

    Create Main.js

---
+++ React/ReactFacts-project/Main.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function Main (){
+    return(
+        <main>       
+             <h1 className="main--title">Fun Facts about React</h1>
+             <ul className="main--facts">
+                <li>was first released in 2013</li>
+                <li>was originally created by Jordan Walke</li>
+                <li>has well over 100k stars on GitHub</li>
+                <li>is maintained by facebook</li>
+                <li>powers thousands of enterprise app, including mobile app</li>
+
+             </ul>
+        </main>
+    )
+}
+
+export default Main
