0ea22ee9876d671ce6f98d240e286a784dbdb33f
saharrshirazii <137921636+saharrshirazii@users.noreply.github.com>
2023-10-31 09:20:31 +0100

    Create App.js

---
+++ React/soldout-badge-project/App.js
@@ -0,0 +1,30 @@
+import React from "react"
+import Navbar from "./Navbar"
+import Card from "./Card"
+import data from "./data"
+
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+                openSpots={item.openSpots}
+            />
+        )
+    })        
+    
+    return (
+        <div>
+            <Navbar />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
+}
