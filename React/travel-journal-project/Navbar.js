db8d86e210ab20a355e706435b99817c2dd64595
saharrshirazii <137921636+saharrshirazii@users.noreply.github.com>
2023-11-02 15:26:55 +0100

    Create Navbar.js

---
+++ React/travel-journal-project/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react"
+import './style.css';
+
+export default function Navbar (){
+return(
+    <nav> 
+        <img src="./images/logo-travel-card.png" className="nav--logo" />
+
+    </nav>
+)
+}
+
+
+
+
+//<img src="./images/airbnb-logo.png" className="nav--logo" />
+
